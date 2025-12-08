8eb23458ad2ea06606ab168649259252a877d5be
AgustinLebed <agus.lebed@gmail.com>
2025-12-08 14:41:46 -0300

    authMiddleware

--- Api/src/routes/authRoutes.ts
+++ Api/src/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { makeAuthController } from "../controllers/makeAuthController";
 import { authService } from "../services/authService";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
@@ -9,6 +10,6 @@ const authController = makeAuthController({ authService: new authService() });
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
-router.get("/me", authController.me);
+router.get("/me", authMiddleware, authController.me);
 
 export default router;
